2eaf6f84a194f83c2fcf46fe368dcf716cdafaf5
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-10-15 16:16:46 -0700

    initial variables in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+// generate random number betweer 19-120
+var randomNumber=Math.floor(Math.random()*102)+19; 
+var wins=0;
+var losses=0;
+var totalScore=0;
+var isFinished= false;
+
+// console log randomNumber
+console.log(randomNumber)
+ 
+    
+
+
+
+//$("#container-rn").text(randomNumber);

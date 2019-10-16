0e2c496969bd369d4a8167ea461894a15f0eab59
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-10-15 18:08:43 -0700

    added to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,26 @@ var randomNumber=Math.floor(Math.random()*102)+19;
 var wins=0;
 var losses=0;
 var totalScore=0;
+var crystalValues=[];
+var crystalPics=["assets/images/image0.jpeg","assets/images/image2.jpeg","assets/images/image1.jpeg","assets/images/image3.jpeg"];
 var isFinished= false;
 
 // console log randomNumber
 console.log(randomNumber)
- 
-    
+
+//how game will know if player is finished playing
+isFinished= true 
+if(totalScore===randomNumber||totalScore>randomNumber);
+
+// resets game if player is finished
+function reset(){
+    if(isFinished){
+    var wins=0;
+    var losses=0;
+    var totalScore=0;
+    var randomNumber=Math.floor(Math.random()*102)+19;//container-rn?
+    var crystalValues=[];
+    }
+};
 
 
-
-//$("#container-rn").text(randomNumber);
